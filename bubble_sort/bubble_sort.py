779b87f722b2e9a5d297af39e59e0a64842fb8c9
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-06-20 14:27:52 +0300

    start

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+    for loop in range(n):
+        for i in range(0, n - 1):
+            if unsorted_list[i] > unsorted_list[i + 1]:
+                temp = unsorted_list[i]
+                unsorted_list[i] = unsorted_list[i + 1]
+                unsorted_list[i + 1] = temp
+    return unsorted_list
+
+# run
+numberArray = [11, 7, 32, 21, 45, 4, 47, 63, 52, 29]
+print(bubble_sort(numberArray))
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for numIndex in range(n - 1):
+        minIndex = numIndex
+        for nextIndex in range(numIndex + 1, n - 1):
+            if arr[nextIndex] < arr[minIndex]:
+                minIndex = nextIndex
+
+        if minIndex != numIndex:
+            temp = arr[numIndex]
+            arr[numIndex] = arr[minIndex]
+            arr[minIndex] = temp
+
+    return arr
+
+
+numberArray = [1, 73, 24, 16, 56, 3, 57, 63, 72, 29]
+print(selection_sort(numberArray))
